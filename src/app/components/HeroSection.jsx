9e6a94bbab1b5fd147eca17e27b97d4e7434a398
Your GitHub Username <elias.mwewah2017@gmail.com>
2024-06-09 12:02:37 +0200

    checking new

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -42,7 +42,7 @@ const HeroSection = () => {
               Hire Me
             </a>
             <a
-              href="/images/projects/my resume.pdf"
+              href="/images/projects/zenith.png"
               download
               className="px-4 py-3 rounded bg-gradient-to-r to-blue-900 from-orange-600 text-white"
             >
