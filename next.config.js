c50ac6eb9cffe52d503b870fc941311c75d2bbe3
Your GitHub Username <elias.mwewah2017@gmail.com>
2024-06-09 11:00:38 +0200

    new retry deployment

--- next.config.js
+++ next.config.js
@@ -1,26 +1,4 @@
-module.exports = {
-    env: {
-      RESEND_API_KEY: process.env.RESEND_API_KEY,
-      FROM_EMAIL: process.env.FROM_EMAIL,
-    },
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-      return config;
-    },
-    webpackDevMiddleware: (config) => {
-      return config;
-    },
-    async headers() {
-      return [
-        {
-          source: "/api/send",
-          headers: [
-            {
-              key: "X-Custom-Header",
-              value: "MyCustomValue",
-            },
-          ],
-        },
-      ];
-    },
-  };
-  
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-      RESEND_API_KEY: process.env.RESEND_API_KEY,
-      FROM_EMAIL: process.env.FROM_EMAIL,
-    },
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {}
+
+module.exports = nextConfig
